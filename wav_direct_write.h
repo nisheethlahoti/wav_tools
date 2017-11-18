0bf4fdb05e1e0c930b65011df1e690bb5587a435
Nisheeth Lahoti <nisheeth.lahoti@gmail.com>
2017-11-18 14:01:39 +0530

    Initial commit. wav_file.h not very efficient

---
+++ wav_direct_write.h
@@ -0,0 +1,53 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * Class for writing to WAVE file as soon as a sample is received. Completes *
+ * proper write on destruction. (File can be in a bad state if not destroyed *
+ * properly.                                                                 *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+#include <cassert>
+
+#include "util.h"
+
+template <typename sample_t>
+class  wav_writer_t {
+	wav_header_t const header;
+	std::ofstream outp;
+	uint32_t count = 0;
+
+public:
+	void write_to_file(char const *filename) {
+	}
+
+	wav_writer_t(char const *filename, wav_header_t const &hdr): outp(filename), header(hdr) {
+		assert(sizeof sample_t() == header.block_align);
+		assert(header.block_align >= header.bits_per_sample / 8 * header.num_ch);
+
+		outp << "RIFF";
+		binary_write<uint32_t>(outp, 0);
+		outp << "WAVE";
+
+		outp << "fmt ";
+		binary_write<uint32_t>(outp, sizeof header);
+		binary_write(outp, header);
+
+		outp << "data";
+		binary_write<uint32_t>(outp, 0);
+	}
+
+	void add_sample(sample_t const &sample) {
+		binary_write(outp, sample);
+		count++;
+	}
+
+	~wav_writer_t() {
+		uint32_t data_size = count * sizeof sample_t();
+		outp.seekp(4, std::ios::beg);
+		binary_write<uint32_t>(outp,
+				4 /*remaining data in this chunk*/
+				+ (8 + sizeof header)/*fmt chunk*/
+				+ (8+data_size)/*data chunk*/);
+		outp.seekp(12 + 8 + sizeof header + 4, std::ios::beg);
+		binary_write(outp, data_size);
+		outp.close();
+	}
+};
---
+++ wav_file.h
@@ -0,0 +1,69 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * Class for reading from and writing to WAVE files. Supports any alignment  *
+ * of data (not necessarily num_channels * byte_depth).                      *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+#include <cassert>
+#include <vector>
+
+#include "util.h"
+
+template <typename sample_t>
+struct wav_t {
+	wav_header_t const header;
+	std::vector<sample_t> samples;
+
+	void write_to_file(char const *filename) {
+		uint32_t data_size = header.block_align * samples.size();
+		std::ofstream outfile(filename, std::ios::binary);
+
+		outfile << "RIFF";
+		binary_write<uint32_t>(outfile,
+				4 /*remaining data in this chunk*/
+				+ (8 + sizeof header)/*fmt chunk*/
+				+ (8+data_size)/*data chunk*/);
+		outfile << "WAVE";
+
+		outfile << "fmt ";
+		binary_write<uint32_t>(outfile, sizeof header);
+		binary_write(outfile, header);
+
+		outfile << "data";
+		binary_write<uint32_t>(outfile, data_size);
+		for (sample_t const &sample: samples) {
+			binary_write(outfile, sample, header.sample_bytes());
+			for (int i=header.sample_bytes(); i<header.block_align; ++i)
+				outfile.put(0);
+		}
+
+		outfile.close();
+	}
+
+	wav_t(const wav_header_t &hdr): header(hdr) {
+		assert(sizeof samples[0] >= header.block_align);
+	}
+};
+
+template<typename sample_t>
+wav_t<sample_t> read_wav(char const fname[]) {
+	wav_header_t header;
+	std::ifstream reader(fname);
+
+	while (true) {
+		auto chunkInfo = readChunkInfo(reader);
+		if (chunkInfo.first == "fmt ") {
+			reader.read(reinterpret_cast<char*>(&header), chunkInfo.second);
+		} else if (chunkInfo.first == "data") {
+			wav_t<sample_t> wav(header);
+			wav.samples.resize(chunkInfo.second / header.block_align);
+			for (auto &smpl: wav.samples) {
+				reader.read(reinterpret_cast<char*>(&smpl), header.sample_bytes());
+				reader.seekg(header.block_align - header.sample_bytes(), std::ios::cur);
+			}
+
+			return wav;
+		} else {
+			reader.seekg(chunkInfo.second, std::ios::cur);
+		}
+	};
+}
